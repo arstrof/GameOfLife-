3baba1a0a833429392e92bfaa81d1fb300a61d9b
arstrof <117746931+arstrof@users.noreply.github.com>
2024-05-29 12:21:24 +0300

    Delete Test_for_Ars_CountingNeighbours.py

--- Test_for_Ars_CountingNeighbours.py
+++
@@ -1,8 +0,0 @@
-import random
-
-list_of_returns = [2, 3, 2, 1, 2, 1, 2, 3, 2]
-
-def CountingNeighbours(XCoordinate, YCoordinate, board, rows, cols, counter):
-    
-    return list_of_returns[counter]
-
